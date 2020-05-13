6c0c426a6ae3cb432706ee1e563ec6f1eed39628
bevexed <547551650@qq.com>
2020-05-13 18:01:51 +0800

    添加注释

--- 数据结构/队列/广度优先搜索/墙与门.js
+++ 数据结构/队列/广度优先搜索/墙与门.js
@@ -1,14 +1,16 @@
 "use strict";
-const EMPTY = Math.pow(2, 31) - 1;
-const GATE = 0;
-const WALL = -1;
+const EMPTY = Math.pow(2, 31) - 1; // 空格
+const GATE = 0; // 门
+const WALL = -1; // 墙
 const DIRECTIONS = [
+    // 要遍历的方向
     [1, 0],
     [-1, 0],
     [0, 1],
     [0, -1],
 ];
-const wallsAndGate = (rooms) => {
+const wallsAndGates = (rooms) => {
+    // 没有房间直接返回
     if (rooms.length === 0 || rooms[0].length === 0) {
         return;
     }
@@ -16,7 +18,6 @@ const wallsAndGate = (rooms) => {
         for (let col = 0; col < rooms[0].length; col++) {
             if (rooms[row][col] === EMPTY) {
                 rooms[row][col] = distanceToNearestGate(rooms, row, col);
-                console.log(rooms);
             }
         }
     }
@@ -24,18 +25,32 @@ const wallsAndGate = (rooms) => {
 const distanceToNearestGate = (rooms, startRow, startCol) => {
     const m = rooms.length;
     const n = rooms[0].length;
-    const distance = new Array(m).fill(new Array(n).fill(0));
     const q = [];
+    // 创建一个 二维数组 用来存放到 传入节点的距离
+    const distance = new Array(m).fill([]).map((item) => {
+        const arr = [];
+        for (let i = 0; i < n; i++) {
+            arr[i] = 0;
+        }
+        return arr;
+    });
     q.push([startRow, startCol]);
     while (q.length !== 0) {
         let [row, col] = q.shift();
         for (let i = 0; i < DIRECTIONS.length; i++) {
             const r = row + DIRECTIONS[i][0];
             const c = col + DIRECTIONS[i][1];
-            if (r < 0 || c < 0 || r >= m || c >= n || rooms[r][c] === WALL) {
+            if (r < 0 || // 判断边界
+                c < 0 || // 判断边界
+                r >= m || // 判断边界
+                c >= n || // 判断边界
+                rooms[r][c] === WALL || // 判断改节点手否是墙
+                distance[r][c] !== 0 || // 判断改节点是否被遍历过
+                (r === startRow && c === startCol) // 判断是否是初始节点
+            ) {
                 continue;
             }
-            distance[r][c] = distance[row][col] + 1;
+            distance[r][c] = distance[row][col] + 1; // 当前距离 等于 上一次遍历的结果 + 1
             if (rooms[r][c] === GATE) {
                 return distance[r][c];
             }
@@ -44,7 +59,7 @@ const distanceToNearestGate = (rooms, startRow, startCol) => {
     }
     return EMPTY;
 };
-wallsAndGate([
+wallsAndGates([
     [2147483647, -1, 0, 2147483647],
     [2147483647, 2147483647, 2147483647, -1],
     [2147483647, -1, 2147483647, -1],
--- 数据结构/队列/广度优先搜索/墙与门.ts
+++ 数据结构/队列/广度优先搜索/墙与门.ts
@@ -1,14 +1,16 @@
-const EMPTY = Math.pow(2, 31) - 1;
-const GATE = 0;
-const WALL = -1;
+const EMPTY = Math.pow(2, 31) - 1; // 空格
+const GATE = 0; // 门
+const WALL = -1; // 墙
 const DIRECTIONS = [
+  // 要遍历的方向
   [1, 0],
   [-1, 0],
   [0, 1],
   [0, -1],
 ];
 
-const wallsAndGate = (rooms: Array<Array<number>>): void => {
+const wallsAndGates = (rooms: Array<Array<number>>): void => {
+  // 没有房间直接返回
   if (rooms.length === 0 || rooms[0].length === 0) {
     return;
   }
@@ -17,7 +19,6 @@ const wallsAndGate = (rooms: Array<Array<number>>): void => {
     for (let col = 0; col < rooms[0].length; col++) {
       if (rooms[row][col] === EMPTY) {
         rooms[row][col] = distanceToNearestGate(rooms, row, col);
-        console.log(rooms);
       }
     }
   }
@@ -30,19 +31,32 @@ const distanceToNearestGate = (
 ): number => {
   const m = rooms.length;
   const n = rooms[0].length;
-  const distance = new Array(m).fill(new Array(n).fill(0));
   const q: number[][] = [];
+  const distance: Array<Array<number>> = new Array(m).fill([]).map((item) => {
+    const arr = [];
+    for (let i = 0; i < n; i++) {
+      arr[i] = 0;
+    }
+    return arr;
+  });
   q.push([startRow, startCol]);
   while (q.length !== 0) {
     let [row, col] = q.shift() as number[];
     for (let i = 0; i < DIRECTIONS.length; i++) {
       const r: number = row + DIRECTIONS[i][0];
       const c: number = col + DIRECTIONS[i][1];
-      if (r < 0 || c < 0 || r >= m || c >= n || rooms[r][c] === WALL) {
+      if (
+        r < 0 ||
+        c < 0 ||
+        r >= m ||
+        c >= n ||
+        rooms[r][c] === WALL ||
+        distance[r][c] !== 0 ||
+        (r === startRow && c === startCol)
+      ) {
         continue;
       }
       distance[r][c] = distance[row][col] + 1;
-
       if (rooms[r][c] === GATE) {
         return distance[r][c];
       }
@@ -52,7 +66,7 @@ const distanceToNearestGate = (
   return EMPTY;
 };
 
-wallsAndGate([
+wallsAndGates([
   [2147483647, -1, 0, 2147483647],
   [2147483647, 2147483647, 2147483647, -1],
   [2147483647, -1, 2147483647, -1],
