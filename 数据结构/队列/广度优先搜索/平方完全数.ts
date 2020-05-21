4746d91c2b911111a2d270f3a6d2cc50b7598e46
bevexed <547551650@qq.com>
2020-05-21 10:27:21 +0800

    完全平方数

---
+++ 数据结构/队列/广度优先搜索/平方完全数.ts
@@ -0,0 +1,39 @@
+const numSquares = function (n: number): number {
+  if (n === 0 || n === 1) {
+    return 1;
+  }
+
+  // 平方数列表集合
+  const powList = [];
+  const targetList = [n, null];
+  const set = new Set();
+  const visited = new Set();
+  const start = Math.floor(Math.sqrt(n));
+  for (let i = 0; i < start; i++) {
+    const val = Math.pow(i + 1, 2);
+    powList.unshift(val);
+    set.add(val);
+  }
+  let step = 1;
+  while (targetList.length) {
+    const cur = targetList.shift() as number;
+    if (set.has(cur)) {
+      return step;
+    }
+    if (cur === null) {
+      step++;
+      targetList.push(null);
+      continue;
+    }
+    for (let i = 0; i < powList.length; i++) {
+      if (cur > powList[i] && !visited.has(cur - powList[i])) {
+        targetList.push(cur - powList[i]);
+        visited.add(cur - powList[i]);
+      }
+    }
+  }
+
+  return 0;
+};
+
+console.log(numSquares(1000));
